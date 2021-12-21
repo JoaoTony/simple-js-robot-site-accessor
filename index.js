8170163a20f838bbdaff15970ea1463b95b27f0b
JoaoTony <joaojosetony2016@gmail.com>
2021-12-21 01:41:52 +0100

    feat: add variable base coin quantity

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 import puppeteer from 'puppeteer';
 import readlineSync from 'readline-sync'
 
-console.log("Bem-vindo ao Robot");
+console.log("Conversor de Moedas");
 
 const robot = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
-  const moedaBase = readlineSync.question('Informe a moeda base: ') || 'dolar';
-  const moedaFinal = readlineSync.question('Informe a moeda final: ') || 'kwanza';
+  const baseCoin = readlineSync.question('Informe a moeda base: ') || 'dolar';
+  const baseCoinQuantity = readlineSync.question('Informe a quantidade(ex: 1): ') || 1;
+  const finalCoin = readlineSync.question('Informe a moeda final: ') || 'kwanza';
 
-  const url = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&sxsrf=AOaemvIH-mQF-jBkc01VxnQ0Aw1-TNXPGQ%3A1640043567719&ei=LxTBYYGsK8SM8gLwmYnYBQ&oq=${moedaBase}+para+kw&gs_lcp=Cgdnd3Mtd2l6EAMYADIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BAgjECc6BAgAEEM6BwgAELEDEEM6BQguEIAEOgYIABAKEEM6CAgAEIAEELEDOggILhCABBCxAzoLCC4QgAQQxwEQ0QM6CQgAEEMQRhCCAjoHCAAQgAQQCkoECEEYAEoECEYYAFAAWLcYYIonaABwAngAgAGsA4gBySaSAQYzLTExLjKYAQCgAQHAAQE&sclient=gws-wiz`;
+  const url = `https://www.google.com/search?q=${baseCoinQuantity}+${baseCoin}+para+${finalCoin}&sxsrf=AOaemvIH-mQF-jBkc01VxnQ0Aw1-TNXPGQ%3A1640043567719&ei=LxTBYYGsK8SM8gLwmYnYBQ&oq=${baseCoin}+para+kw&gs_lcp=Cgdnd3Mtd2l6EAMYADIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BAgjECc6BAgAEEM6BwgAELEDEEM6BQguEIAEOgYIABAKEEM6CAgAEIAEELEDOggILhCABBCxAzoLCC4QgAQQxwEQ0QM6CQgAEEMQRhCCAjoHCAAQgAQQCkoECEEYAEoECEYYAFAAWLcYYIonaABwAngAgAGsA4gBySaSAQYzLTExLjKYAQCgAQHAAQE&sclient=gws-wiz`;
   await page.goto(url);
   await page.screenshot({ path: 'example.png' });
 
-  const getResultValueFromNavegator = await page.evaluate(() =>  
+  const getResultValueFromBrowser = await page.evaluate(() =>
     document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc").value
   );
 
-  const result = getResultValueFromNavegator;
+  const result = getResultValueFromBrowser;
 
-  console.log(`O valor de 1 ${moedaBase} em ${moedaFinal} é ${result}`);
+  if(!result) {
+    console.log("Alguma coisa deu errado!");
+  } else {
+    console.log(`O valor de ${baseCoinQuantity} ${baseCoin} em ${finalCoin} é ${result}`);
+  }
+
   await browser.close();
 }
 
