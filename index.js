af1d309f1e16be45ece6dd9dc7d4e1d56a290be9
JoaoTony <joaojosetony2016@gmail.com>
2021-12-21 02:23:58 +0100

    feat: add error prevention when inserted a wrong coin

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 import puppeteer from 'puppeteer';
 import readlineSync from 'readline-sync'
 
-console.log("Conversor de Moedas");
+console.log("CONVERSOR DE MOEDAS");
+
+const getResultValueFromBrowser = async page => {
+  return await page.evaluate(() => {
+    const $value = document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc");
+      return $value?.value;
+  })
+}
 
 const robot = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
-  const baseCoin = readlineSync.question('Informe a moeda base: ') || 'dolar';
+  const baseCoin = readlineSync.question('Informe a moeda base(ex: dolar): ') || 'dolar';
   const baseCoinQuantity = readlineSync.question('Informe a quantidade(ex: 1): ') || 1;
-  const finalCoin = readlineSync.question('Informe a moeda final: ') || 'kwanza';
+  const finalCoin = readlineSync.question('Informe a moeda final(ex: kwanza): ') || 'kwanza';
 
   const url = `https://www.google.com/search?q=${baseCoinQuantity}+${baseCoin}+para+${finalCoin}&sxsrf=AOaemvIH-mQF-jBkc01VxnQ0Aw1-TNXPGQ%3A1640043567719&ei=LxTBYYGsK8SM8gLwmYnYBQ&oq=${baseCoin}+para+kw&gs_lcp=Cgdnd3Mtd2l6EAMYADIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BAgjECc6BAgAEEM6BwgAELEDEEM6BQguEIAEOgYIABAKEEM6CAgAEIAEELEDOggILhCABBCxAzoLCC4QgAQQxwEQ0QM6CQgAEEMQRhCCAjoHCAAQgAQQCkoECEEYAEoECEYYAFAAWLcYYIonaABwAngAgAGsA4gBySaSAQYzLTExLjKYAQCgAQHAAQE&sclient=gws-wiz`;
   await page.goto(url);
-  await page.screenshot({ path: 'example.png' });
 
-  const getResultValueFromBrowser = await page.evaluate(() =>
-    document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc").value
-  );
-
-  const result = getResultValueFromBrowser;
-
+  const result = await getResultValueFromBrowser(page);
+  
   if(!result) {
-    console.log("Alguma coisa deu errado!");
+    console.log("Alguma coisa deu errado, certifica-se de informar as moedas e a quantidade corretamente. ");
   } else {
     console.log(`O valor de ${baseCoinQuantity} ${baseCoin} em ${finalCoin} é ${result}`);
   }
